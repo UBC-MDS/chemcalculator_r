9805d2ccd3f3aa3858b06a2342e47a960deaac71
datallurgy <allyson@datallurgy.com>
2022-02-02 17:59:45 -0800

    noRd for helper functions

--- R/chemcalculator.R
+++ R/chemcalculator.R
@@ -135,8 +135,7 @@ percent_mass <- function(compound, element) {
 #'
 #' @return Dataframe of the chemicals elemental components and their counts.
 #' @import dplyr stringr
-#' @keywords internal
-#' @NoRd
+#' @noRd
 #'
 .chemical_elements <- function(chemical) {
 
@@ -234,8 +233,7 @@ percent_mass <- function(compound, element) {
 #' @param chemical chemical formula to check
 #'
 #' @return True or FALSE for if chemical formula has the correct format
-#' @keywords internal
-#' @NoRd
+#' @noRd
 #'
 #' @examples
 #' .check_chemical_format("H2O")          ## True
